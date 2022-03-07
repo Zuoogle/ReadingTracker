9ed179aaefa0f2909cec230ce02c35f428a62935
Sherefedin M Getahun <smgetahun@live.com>
2022-03-06 23:07:13 -0800

    Adding new record not working

--- src/App.js
+++ src/App.js
@@ -1,45 +1,51 @@
+import React, { useState } from 'react';
 import NewQuranTest from './components/NewQuranTest/NewQuranTest';
 import Tests from './components/QuranTests/QuranTests';
 
+
+const Initial_Test_Result = [
+  {
+    id: '1',
+    childName: 'Abdullah Mensur',
+    surah: 'Al-Beqerah',
+    date: new Date(2021, 7, 25),
+    startAyah: '45',
+    endAyah: '55',
+    status: 'Fail'
+  },
+  {
+    id: '2',
+    childName: 'Abdurehman Mensur',
+    surah: 'Al-Imran',
+    date: new Date(2020, 2, 12),
+    startAyah: '120',
+    endAyah: '245',
+    status: 'Pass'
+  },
+  {
+    id: '3',
+    childName: 'Hebatullah Mensur',
+    surah: 'An-Nissa',
+    date: new Date(2022, 2, 6),
+    startAyah: '100',
+    endAyah: '109',
+    status: 'Pass'
+  }
+];
+
 const App = () => {
-  const testResult = [
-    {
-      id: '1',
-      childName: 'Abdullah Mensur',
-      surah: 'Al-Beqerah',
-      date: new Date(2021, 7, 25),
-      startAyah: '45',
-      endAyah: '55',
-      status: 'Fail'
-    },
-    {
-      id: '2',
-      childName: 'Abdurehman Mensur',
-      surah: 'Al-Imran',
-      date: new Date(2020, 2, 12),
-      startAyah: '120',
-      endAyah: '245',
-      status: 'Pass'
-    },
-    {
-      id: '3',
-      childName: 'Hebatullah Mensur',
-      surah: 'An-Nissa',
-      date: new Date(2022, 2, 6),
-      startAyah: '100',
-      endAyah: '109',
-      status: 'Pass'
-    }
-  ];
-  const addQuranTestHandler= quranTest =>{
-    console.log('In App.js');
-    console.log(quranTest);
+
+  const [tests, setTests] = useState(Initial_Test_Result);
+
+  const addQuranTestHandler = (quranTest) => {
+    setTests((prevTests) => {
+      return [quranTest, ...prevTests];
+    });
   }
   return (
     <div className="App">
-      <NewQuranTest onAddQuranTest={addQuranTestHandler}/>
-      
-      <Tests result={testResult} />
+      <NewQuranTest onAddQuranTest={addQuranTestHandler} />
+      <Tests result={tests} />
     </div >
   );
 }
--- src/components/QuranTests/QuranTests.js
+++ src/components/QuranTests/QuranTests.js
@@ -7,7 +7,7 @@ import './QuranTests.css';
 const QuranTests = (props) => {
     const [filteredYear, setFilteredYear] = useState('2020');
 
-    const filterChangeHandler = selectedYear => {
+    const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
     }
 
@@ -17,33 +17,15 @@ const QuranTests = (props) => {
                 <QuranTestFilter
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler} />
-                <QuranTestItem
-                    id={props.result[0].id}
-                    childName={props.result[0].childName}
-                    surah={props.result[0].surah}
-                    date={props.result[0].date}
-                    startAyah={props.result[0].startAyah}
-                    endAyah={props.result[0].endAyah}
-                    status={props.result[0].status}
-                />
-                <QuranTestItem
-                    id={props.result[1].id}
-                    childName={props.result[1].childName}
-                    surah={props.result[1].surah}
-                    date={props.result[1].date}
-                    startAyah={props.result[1].startAyah}
-                    endAyah={props.result[1].endAyah}
-                    status={props.result[1].status}
-                />
-                <QuranTestItem
-                    id={props.result[2].id}
-                    childName={props.result[2].childName}
-                    surah={props.result[2].surah}
-                    date={props.result[2].date}
-                    startAyah={props.result[2].startAyah}
-                    endAyah={props.result[2].endAyah}
-                    status={props.result[2].status}
-                />
+                {
+                    props.result.map((tests) => (<QuranTestItem
+                            childName={tests.childName}
+                            surah={tests.surah}
+                            startAyah={tests.startAyah}
+                            endAyah={tests.endAyah}
+                            date={tests.date}
+                        />))
+                }
             </Card>
         </div>
     );
